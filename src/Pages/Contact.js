911348e61f63db4b5f10c9ecc757b1be6b3b0da0
Ina Kristine <inakristine@gmail.com>
2020-04-23 22:17:43 +0200

    Added form-styling

--- src/Pages/Contact.js
+++ src/Pages/Contact.js
@@ -1,101 +1,134 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
+import HomeTitle from '../components/homeTitles';
 
 export default function ContactSpecific() {
     let [success, setSuccess] = useState(false);
-    let [firstName, setFirstName] = useState('');
-    let [lastName, setLastName] = useState('');
+    let [name, setname] = useState('');
+    let [companyName, setCompanyName] = useState('');
     let [email, setEmail] = useState('');
-    let [phoneNum, setPhoneNum] = useState('');
+    let [messageTitle, setMessageTitle] = useState('');
+    let [message, setmessage] = useState('');
     let emailPattern = /^([A-Za-z0-9_\-.])+@([A-Za-z0-9_\-.])+.([A-Za-z]{2,4})$/;
-    let phonePattern = /^([0-9]{9})$/;
 
-    // Both Thess patterns works just fine in a regex-tester online, AND in anoter website, but not in this one :-/
-    // let phonePattern = /([0-9]{3}[-|\s|.])([0-9]{3}[-|\s|.])([0-9]{3})$/;
-    // let phonePattern = /^\d{3}([\-\s\.])?\d{3}([\-\s\.])?\d{3}$/g;
+
 
     const HandleChange = input => {
         let name = input.target.name;
         let value = input.target.value;
 
         switch (name) {
-            case 'firstname':
-                setFirstName(value);
+            case 'name':
+                setname(value);
                 break;
-            case 'lastname':
-                setLastName(value);
+            case 'companyName':
+                setCompanyName(value);
                 break;
             case 'email':
                 setEmail(value);
                 break;
-            case 'phoneNumber':
-                setPhoneNum(value);
+            case 'messageTitle':
+                setMessageTitle(value);
+                break;
+            case 'messageber':
+                setmessage(value);
                 break;
             default:
                 break;
         }
     };
 
-    const HandleSubmit = (evt) => {
-        evt.preventDefault();
+
+    const HandleSubmit= (e) =>{
+        e.preventDefault();
+    
         setSuccess(!success);
-        console.log(firstName);
-        console.log(lastName);
+        console.log(name);
+        console.log(companyName);
         console.log(email);
-        console.log(phoneNum);
-    }
+        console.log(messageTitle);
+        console.log(message);
+        
+
+        axios({
+          method: "POST", 
+          url:"http://localhost:3002/send", 
+          data:  this.state
+        }).then((response)=>{
+          if (response.data.status === 'success'){
+            alert("Message Sent."); 
+            this.resetForm()
+          }else if(response.data.status === 'fail'){
+            alert("Message failed to send.")
+          }
+        })
+      }
+
 
     return (
-        <div className='pageContent'>
-            <div className='col-sm-6'>
-                <h1>Contact form</h1>
-                <form onSubmit={HandleSubmit} >
+        <div className='contact'>
+        <HomeTitle theme='Contact me:' />
+            <form className='contact__form' onSubmit={HandleSubmit} >
+                
+            <div className="contact__form--name">
+                <h3>Please enter your name:</h3>
+                <input className='contact__input' type='text'
+                    name='name'
+                    value={name}
+                    onChange={HandleChange}
+                />
+                <p className={(name === '') ? 'error' : 'error__hide'}>Tha name field can not be empty</p>
+                </div>
 
-                    <p>Enter your first name</p>
-                    <input type='text'
-                        name='firstname'
-                        className='form-controll'
-                        value={firstName}
-                        onChange={HandleChange}
-                    />
-                    <p className={(firstName === '') ? 'error' : 'error__hide'}>Please enter a first name</p>
+                <div className="contact__form--companyName">
+                <h3>Please enter your company name:</h3>
+                <input className='contact__input' type='text'
+                    name='companyName'
+                    value={companyName}
+                    onChange={HandleChange}
+                />
+                <p className={(companyName === '') ? 'error' : 'error__hide'}>The company name field can not be empty</p>
+                </div>
 
-                    <p>Enter your last name</p>
-                    <input type='text'
-                        name='lastname'
-                        className='form-controll'
-                        value={lastName}
-                        onChange={HandleChange}
-                    />
-                    <p className={(lastName === '') ? 'error' : 'error__hide'}>Please enter a last name</p>
+                <div className="contact__form--email">
+                <h3>Please enter your email adress:</h3>
+                <input className='contact__input' type='text'
+                    name='email'
+                    value={email}
+                    onChange={HandleChange}
+                />
+                <p className={(emailPattern.test(email)) ? 'error__hide' : 'error'}>Please input valid an email adress</p>
+                </div>
 
+                <div className="contact__form--mTitile">
+                <h3>Please enter a message title:</h3>
+                <input className='contact__input' type='text'
+                    name='messageTitle'
+                    value={messageTitle}
+                    onChange={HandleChange}
+                />
+                <p className={(messageTitle === '') ? 'error' : 'error__hide'}>The message title field can not be empty</p>
+                </div>
 
-                    <p>Enter your email adress</p>
-                    <input type='text'
-                        name='email'
-                        className='form-controll'
-                        value={email}
-                        onChange={HandleChange}
-                    />
-                    <p className={(emailPattern.test(email)) ? 'error__hide' : 'error'}>Please enter an email name</p>
-
-                    <p>Enter your Phone number</p>
-                    <input type='number'
-                        name='phoneNumber'
-                        className='form-controll'
-                        value={phoneNum}
-                        onChange={HandleChange}
-                    />
-                    <p className={(phonePattern.test(phoneNum)) ? 'error__hide' : 'error'}>Please enter 9 digits</p>
-                    <br />
-
-                    <br />
-                    <input type='submit' disabled={(firstName === '') || (lastName === '') || (emailPattern.test(email) === false) || (phonePattern.test(phoneNum) === false)} className='btn' />
-                </form>
-                {
-                    (success === true) ? <div><h4>Yor input was successfully submitted</h4></div> : <div>This button activates when your form is filled out correct.</div>
-                }
-
-            </div>
+                <div className="contact__form--message">
+                <h3>Enter a message</h3>
+                <textarea className='contact__input--message' type='text'
+                    rows='15' cols='95'
+                    name='messageber'
+                    value={message}
+                    onChange={HandleChange}
+                />
+                <p className={(message === '') ? 'error' : 'error__hide'}>The message field can not be empty</p>
+                </div>
+                <div className="contact__form--submit">
+                <br />
+                <input type='submit' disabled={(name === '') || (companyName === '') || (emailPattern.test(email) === false) || (message === '') || (messageTitle === '')} className='contact__submit' />
+                </div>
+            </form>
+            {
+                (success === true) ? <div className='contact__success'><h3>Yor input was successfully submitted</h3></div> : <div className='contact__success'><p>This button activates when your form is filled out correct.</p></div>
+            }
         </div>
     )
 
