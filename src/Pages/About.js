25a72e22b4f09ddde5099bf635a4d790743b2d52
Ina Kristine <inakristine@gmail.com>
2020-04-25 16:14:09 +0200

    Content in added in About-page

--- src/Pages/About.js
+++ src/Pages/About.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import HomeTitle from '../components/generalTitle';
+import Card from '../components/contactCard';
+import Paragraph from '../components/paragraph';
 
 export default function contact(props) {
 
@@ -7,15 +9,30 @@ export default function contact(props) {
   return (
     <div>
     <HomeTitle theme='About me:' />
-    <conactCard />
+    <Card />
       
     <HomeTitle theme='My key skills:' />
+    <Paragraph text='• Vocational education in both graphic design and frontend developmen' />
+    <Paragraph text='• Unique experience in the interface between marketing and publishing' />
+    <Paragraph text='• Takes challenges and learns quickly' />
+    <Paragraph text='• Fearless in the face of new challenges' />
+    <Paragraph text='• Allways solution-oriented' />
     
     <HomeTitle theme='Something Personal:' />
+    <Paragraph text='I have the opportunity to work very flexibly without regard to sick children or kindergarten opening hours. My husband has a job situation that allows him to take care of that bit. I am also good at baking cakes, and organizing events. I am practical and have an eye for logistics.' />
     
     <HomeTitle theme='Strong sides:' />
-      
+    <Paragraph text='I have a great love for learning new skills and making products of value to my clients.
+    As a classic nerd, I don’t like to put my unresolved issues aside. I am efficient, conscientious, educated and have a lot of loyalty and good work ethic to offer.' />
+    
     <HomeTitle theme='My aspirations:' />
+    <Paragraph text='My perfect workplace is a place where knowledge is shered goods, and where my collegues and I find plesure in shering new skills and keeping each other up to date in new front-end  tecniques. It is a place where employees professional development is highly valued so that they can fulfill their dayly tasks in the best possible way.' />
+
+    <Paragraph text='I would love the oppertunity to come to work and meet my collegues every day. I can work from home, but where is the fun in that?' />
+
+    <Paragraph text='Ideally, I want to work in a place where the working environment is good and where I can go to work and be inspired by good colleagues.
+    A positive and inspiering working environment, where I can evolve as an employee, and feel secure and safe is more important to me than fancy perks and high salaries.' />
+    
 
 
 
